437b6e96dd804587f5bd9e9cd0fa598a97b06afb
ToasterUwU <toasterger@gmail.com>
2023-01-17 19:30:27 +0100

    formatting, nothing more

--- python/src/roman_to_integer.py
+++ python/src/roman_to_integer.py
@@ -1,14 +1,6 @@
 # https://leetcode.com/problems/roman-to-integer/
 class Solution:
-    numeral_values = {
-        "I": 1,
-        "V": 5,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000
-    }
+    numeral_values = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
     numeral_order = [x for x in numeral_values]
 
@@ -22,7 +14,10 @@ class Solution:
             else:
                 next_numeral = None
 
-            if next_numeral != None and (self.numeral_order.index(current_numeral) < self.numeral_order.index(next_numeral)):
+            if next_numeral != None and (
+                self.numeral_order.index(current_numeral)
+                < self.numeral_order.index(next_numeral)
+            ):
                 value -= self.numeral_values[current_numeral]
             else:
                 value += self.numeral_values[current_numeral]
