7a9bbd073256d4ad6397f5b5ceceb39f6378ac14
ToasterUwU <toasterger@gmail.com>
2023-01-21 20:24:58 +0100

    this now also works, until it doesnt anymore (with negative values like before)
    I simply dont understand how i can prevent it, since im not entirely sure why this even happens. Would be easier if the tests are visible and dont get cut off. I cant know whats wrong if its like "You returned xyz..., but i wanted xyz...". "..." being a "there is more, but i wont show all of it" kinda thing.

--- rust/src/fancy_sequence.rs
+++ rust/src/fancy_sequence.rs
@@ -1,10 +1,14 @@
 struct Fancy {
     data: Vec<i128>, //i128 for maximum size
+    operations: Vec<(char, i128, usize)>,
 }
 
 impl Fancy {
     fn new() -> Self {
-        return Fancy { data: vec![] };
+        return Fancy {
+            data: vec![],
+            operations: vec![],
+        };
     }
 
     fn append(&mut self, val: i32) {
@@ -12,22 +16,34 @@ impl Fancy {
     }
 
     fn add_all(&mut self, inc: i32) {
-        for i in 0..self.data.len() { //to every entry, add inc
-            self.data[i] += inc as i128;
-        }
+        self.operations
+            .push(('+', inc as i128, self.data.len() - 1)); // add "+ inc" to the list of operations todo, apply only on current data
     }
 
     fn mult_all(&mut self, m: i32) {
-        for i in 0..self.data.len() { // multiply every entry by m
-            self.data[i] *= m as i128;
-        }
+        self.operations.push(('*', m as i128, self.data.len() - 1)); // add "* m" to the list of operations todo, apply only on current data
     }
 
     fn get_index(&self, idx: i32) -> i32 {
-        if idx as usize >= self.data.len() { //if out of bounds, return -1
+        if idx as usize >= self.data.len() {
+            //if out of bounds, return -1
             return -1;
         }
 
-        return (self.data[idx as usize] % 1000000007) as i32; //return value with modulo 10^9 + 7
+        let mut value = self.data[idx as usize];
+        for (operation, amount, until_index) in self.operations.clone() {
+            if idx as usize > until_index {
+                // if operation came before data, ignore step
+                continue;
+            }
+
+            if operation == '+' {
+                value += amount
+            } else {
+                value *= amount
+            }
+        }
+
+        return (value % 1000000007) as i32; //return value with modulo 10^9 + 7
     }
 }
