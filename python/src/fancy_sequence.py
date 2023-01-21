ac67aa853e9d55b08715ab2077c8d58a2516b444
ToasterUwU <toasterger@gmail.com>
2023-01-21 19:56:44 +0100

    this works, is faster, but not fast enough, will try with rust again

--- python/src/fancy_sequence.py
+++ python/src/fancy_sequence.py
@@ -3,19 +3,33 @@ class Fancy:
     def __init__(self):
         self.data = []  # make list as underlying datastructure
 
+        self.operations = []  # make a list to save the todo operation in
+
     def append(self, val: int) -> None:
         self.data.append(val)  # append to data
 
     def addAll(self, inc: int) -> None:
-        for i in range(len(self.data)):  # increase each value
-            self.data[i] += inc
+        self.operations.append(
+            ("+", inc, len(self.data) - 1)
+        )  # add "+ inc" to the list of operations todo, apply only on current data
 
     def multAll(self, m: int) -> None:
-        for i in range(len(self.data)):  # multiply each value
-            self.data[i] *= m
+        self.operations.append(
+            ("*", m, len(self.data) - 1)
+        )  # add "* m" to the list of operations todo, apply only on current data
 
     def getIndex(self, idx: int) -> int:
         if idx >= len(self.data):  # if out of bounds, return -1
             return -1
 
-        return self.data[idx] % 1000000007  # else return value with modulo 10⁹ + 7
+        value = self.data[idx]  # get value, and perform all operations on it
+        for operation, amount, until_index in self.operations:
+            if idx > until_index:  # if operation came before data, ignore step
+                continue
+
+            if operation == "+":
+                value += amount
+            else:
+                value *= amount
+
+        return value % 1000000007  # return value with modulo 10⁹ + 7
